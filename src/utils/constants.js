6c00c84533079c761f596a3bc22f24b790fc9a53
Ismail <ismailsaad03@gmail.com>
2021-02-18 17:37:42 +0100

    Starter project files setup

---
+++ src/utils/constants.js
@@ -0,0 +1,37 @@
+export const navLinks = [
+  {
+    id: 1,
+    link: 'new in',
+    url: '/',
+  },
+  {
+    id: 2,
+    link: 'designer',
+    url: '/',
+  },
+  {
+    id: 3,
+    link: 'clothing',
+    url: '/',
+  },
+  {
+    id: 4,
+    link: 'footwear',
+    url: '/',
+  },
+  {
+    id: 5,
+    link: 'bags',
+    url: '/',
+  },
+  {
+    id: 6,
+    link: 'accessories',
+    url: '/',
+  },
+  {
+    id: 7,
+    link: 'sale',
+    url: '/',
+  },
+];
