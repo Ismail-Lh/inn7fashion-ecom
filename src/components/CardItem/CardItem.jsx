d2ceb69918137c0986e5a300bf7e0fc37517147b
Ismail <ismailsaad03@gmail.com>
2021-02-26 16:24:49 +0100

    Add the hover effect to the CardItem compo

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -6,27 +6,31 @@ import { formatPrice } from '../../utils/helpers';
 const CardItem = ({ name, designer, price, discountPer, color, images }) => {
   const salePrice = (price * discountPer) / 100;
 
-  const [img, setImg] = useState(images[0]);
+  // const [img, setImg] = useState(images[0]);
 
   return (
     <div className='card'>
-      <img
-        alt={`${designer}-${name}`}
-        className='card__img'
-        onMouseEnter={() => setImg(images[1])}
-        onMouseLeave={() => setImg(images[0])}
-        src={img}
-      />
+      <div className='card__img'>
+        <img
+          src={images[0]}
+          alt={`${designer}-${name}-1`}
+          className='card__img-1'
+        />
+        <img
+          src={images[1]}
+          alt={`${designer}-${name}-2`}
+          className='card__img-2'
+        />
+        <div className='icons'>
+          <i class='fas fa-search' />
+          <i class='far fa-heart' />
+        </div>
+      </div>
       <h3 className='card__name'>{name}</h3>
       <p className='card__designer'>{designer}</p>
       <p className='card__price'>
         <span>{formatPrice(price)}</span> <span>{formatPrice(salePrice)}</span>{' '}
       </p>
-
-      <div className='icons'>
-        <i class='fas fa-search' />
-        <i class='far fa-heart' />
-      </div>
     </div>
   );
 };
